7b155a70237aac6a58e41f4d1c765b8c5e528f7d
ramasheshasai <ramasheshasai_s@srmap.edu.in>
2025-10-01 15:37:03 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,11 @@
+const num = 10;
+if(num < 10){
+    console.log('Small Number');
+}
+else {
+    console.log('large number ');
+}
+
+console.log(`hey it's my first node app!!!`);
+
+// To run this file use the command: node intro.js
